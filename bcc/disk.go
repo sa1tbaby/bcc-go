e36afb2f933f984a34c3d9a3945e53c4deb352fc
sa1tbaby <erpenteleychuk@basistech.ru>
2025-12-09 15:00:08 +0300

    update: refactoring disk and add additional logging

--- bcc/disk.go
+++ bcc/disk.go
@@ -2,8 +2,22 @@ package bcc
 
 import (
 	"fmt"
+	"log"
 	"net/url"
+
+	"github.com/pkg/errors"
 )
+
+type TmpVm struct {
+	manager  *Manager
+	ID       string  `json:"id"`
+	Name     string  `json:"name"`
+	Cpu      int     `json:"cpu"`
+	Ram      float64 `json:"ram"`
+	Power    bool    `json:"power"`
+	Platform string  `json:"platform,omitempty"`
+	Vdc      *Vdc    `json:"vdc"`
+}
 
 type Disk struct {
 	manager        *Manager
@@ -13,7 +27,8 @@ type Disk struct {
 	ExternalID     string          `json:"external_id"`
 	IsRoot         bool            `json:"is_root"`
 	Size           int             `json:"size"`
-	Vm             *Vm             `json:"vm"`
+	Vdc            *Vdc            `json:"vdc,omitempty"`
+	Vm             *TmpVm          `json:"vm"`
 	StorageProfile *StorageProfile `json:"storage_profile"`
 	Locked         bool            `json:"locked,omitempty"`
 	Tags           []Tag           `json:"tags"`
@@ -47,12 +62,45 @@ func (v *Vdc) GetDisks(extraArgs ...Arguments) (disks []*Disk, err error) {
 
 func (m *Manager) GetDisk(id string) (disk *Disk, err error) {
 	path, _ := url.JoinPath("v1/disk", id)
-	err = m.Get(path, Defaults(), &disk)
-	if err != nil {
-		return
+	if err = m.Get(path, Defaults(), &disk); err != nil {
+		log.Printf("[REQUEST-ERROR]: getting disk-%s was failed: %s]", id, errors.WithStack(err))
+		return disk, err
+	} else {
+		disk.manager = m
+		return disk, nil
 	}
-	disk.manager = m
-	return
+}
+
+func (v *Vdc) CreateDisk(disk *Disk) error {
+	args := &struct {
+		Name           string   `json:"name"`
+		Vdc            *string  `json:"vdc,omitempty"`
+		Vm             *string  `json:"vm,omitempty"`
+		Size           int      `json:"size"`
+		StorageProfile string   `json:"storage_profile"`
+		Tags           []string `json:"tags"`
+	}{
+		Name:           disk.Name,
+		Vdc:            &v.ID,
+		Vm:             nil,
+		Size:           disk.Size,
+		StorageProfile: disk.StorageProfile.ID,
+		Tags:           convertTagsToNames(disk.Tags),
+	}
+
+	if disk.Vm != nil {
+		args.Vm = &disk.Vm.ID
+		args.Vdc = nil
+	}
+
+	if err := v.manager.Request("POST", "v1/disk", args, &disk); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for create disk-%s was failed", disk.Name)
+	} else {
+		disk.manager = v.manager
+	}
+
+	return nil
 }
 
 func (v *Vm) AttachDisk(disk *Disk) error {
@@ -64,11 +112,12 @@ func (v *Vm) AttachDisk(disk *Disk) error {
 		Vm: v.ID,
 	}
 
-	err := v.manager.Request("POST", path, args, nil)
-	if err != nil {
-		return err
+	if err := v.manager.Request("POST", path, args, nil); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for attaching disk-%s was failed", disk.ID)
+	} else {
+		v.Disks = append(v.Disks, disk)
 	}
-	v.Disks = append(v.Disks, disk)
 
 	return nil
 }
@@ -76,17 +125,27 @@ func (v *Vm) AttachDisk(disk *Disk) error {
 func (v *Vm) DetachDisk(disk *Disk) error {
 
 	path := fmt.Sprintf("v1/disk/%s/detach", disk.ID)
-	err := v.manager.Request("POST", path, nil, nil)
-	if err != nil {
-		return err
-	}
-	for i, vmDisk := range v.Disks {
-		if vmDisk == disk {
-			v.Disks = append(v.Disks[:i], v.Disks[i+1:]...)
-			break
+	if err := v.manager.Request("POST", path, nil, nil); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for detaching disk-%s was failed", disk.ID)
+	} else {
+		for i, vmDisk := range v.Disks {
+			if vmDisk == disk {
+				v.Disks = append(v.Disks[:i], v.Disks[i+1:]...)
+				break
+			}
 		}
 	}
 
+	return nil
+}
+
+func (d *Disk) UpdateStorageProfile(storageProfile StorageProfile) error {
+	d.StorageProfile = &storageProfile
+	if err := d.Update(); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for update storage-profile %s was failed", err)
+	}
 	return nil
 }
 
@@ -107,7 +166,8 @@ func (d *Disk) Update() error {
 
 	err := d.manager.Request("PUT", path, args, d)
 	if err != nil {
-		return err
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for update-disk-%s was failed", d.ID)
 	}
 
 	return nil
@@ -120,20 +180,30 @@ func (d *Disk) Rename(name string) error {
 
 func (d *Disk) Resize(size int) error {
 	d.Size = size
-	return d.Update()
-}
-
-func (d *Disk) UpdateStorageProfile(storageProfile StorageProfile) error {
-	d.StorageProfile = &storageProfile
-	return d.Update()
+	if err := d.Update(); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for resize disk was failed.")
+	} else {
+		return nil
+	}
 }
 
 func (d *Disk) Delete() error {
 	path, _ := url.JoinPath("v1/disk", d.ID)
-	return d.manager.Delete(path, Defaults(), nil)
+	if err := d.manager.Delete(path, Defaults(), nil); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for delete-disk-%s was failed", d.ID)
+	} else {
+		return nil
+	}
 }
 
-func (d Disk) WaitLock() (err error) {
+func (d Disk) WaitLock() error {
 	path, _ := url.JoinPath("v1/disk", d.ID)
-	return loopWaitLock(d.manager, path)
+	if err := loopWaitLock(d.manager, path); err != nil {
+		log.Printf("[REQUEST-ERROR]: %s]", errors.WithStack(err))
+		return errors.Wrapf(err, "Request for WaitLock disk was failed")
+	} else {
+		return nil
+	}
 }
